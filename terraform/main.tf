f64256e3848fb0b810f90f5d809257afc83b8381
christian.arroyo <chris.cap22@gmail.com>
2024-09-02 17:02:29 -0700

    updated terraform main file

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,13 +11,29 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "example-bucket"
+resource "aws_s3_bucket" "my_s3_bucket" {
+  bucket = "christian-arroyo-bucket-001-2"
 }
 
-resource "aws_s3_bucket_versioning" "my_bucket" {
-  bucket = aws_s3_bucket.example.id
+resource "aws_s3_bucket_versioning" "my_s3_bucket" {
+  bucket = aws_s3_bucket.my_s3_bucket.id
   versioning_configuration {
     status = "Enabled"
   }
+}
+
+resource "aws_iam_user" "my_iam_user" {
+    name = "my_iam_user_abc_updated1"
+}
+
+output "my_s3_bucket_versioning" {
+    value = aws_s3_bucket.my_s3_bucket.versioning[0].enabled
+}
+
+output "my_s3_bucket_complete_details" {
+    value = aws_s3_bucket.my_s3_bucket
+}
+
+output "my_iam_user_complete_details" {
+    value = aws_iam_user.my_iam_user
 }
